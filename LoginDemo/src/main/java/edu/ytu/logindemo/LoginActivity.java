ed09e88160f88f9747269094dac8c561bae47e5c
etoyz <2905226519@qq.com>
2022-03-11 21:30:01 +0800

    模拟登录

--- LoginDemo/src/main/java/edu/ytu/logindemo/LoginActivity.java
+++ LoginDemo/src/main/java/edu/ytu/logindemo/LoginActivity.java
@@ -15,9 +15,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         application = (MyApplication) getApplication();
         setContentView(R.layout.activity_login);
-
         findViewById(R.id.agree_and_continue).setOnClickListener(view -> {
             String mobileNumber = ((TextView) LoginActivity.this.findViewById(R.id.phone_input)).getText().toString();
+            String password = ((TextView) LoginActivity.this.findViewById(R.id.password_input)).getText().toString();
             if (application.verifyMobileNumber(mobileNumber)) {
                 application.showToast("正在登录，" + mobileNumber, Toast.LENGTH_SHORT);
                 try {
@@ -25,8 +25,12 @@ public class LoginActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(intent);
+                if (application.login(mobileNumber, password)) {
+                    application.showToast("欢迎回来！", Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    LoginActivity.this.startActivity(intent);
+                } else
+                    application.showToast("密码错误！", Toast.LENGTH_SHORT);
             } else {
                 application.showToast("请输入正确的手机号！", Toast.LENGTH_LONG);
             }
--- LoginDemo/src/main/java/edu/ytu/logindemo/MyApplication.java
+++ LoginDemo/src/main/java/edu/ytu/logindemo/MyApplication.java
@@ -1,13 +1,27 @@
 package edu.ytu.logindemo;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyApplication extends Application {
     private static Toast toast;
+    private static SharedPreferences credentials;
+    private static Map<String, String> tmpUsers = new HashMap<>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        credentials = this.getSharedPreferences("CREDENTIALS", Context.MODE_PRIVATE);
+        tmpUsers.put("11111111111", "111111");
+        tmpUsers.put("22222222222", "222222");
+    }
 
     protected void showToast(String text, int duration) {
         if (toast != null)
@@ -20,5 +34,28 @@ public class MyApplication extends Application {
         Pattern p = Pattern.compile("^[0-9]{11}$");
         Matcher matcher = p.matcher(s);
         return matcher.matches();
+    }
+
+    protected boolean checkLoginStatus() {
+        String id = credentials.getString("id", "");
+        String password = credentials.getString("password", "");
+
+        // 调用接口验证
+        if (tmpUsers.containsKey(id) && tmpUsers.containsValue(password))
+            return true;
+        else
+            return false;
+    }
+
+    protected void logout() {
+        credentials.edit().remove("id").remove("password").apply();
+    }
+
+    protected boolean login(String id, String password) {
+        if (tmpUsers.containsKey(id) && tmpUsers.containsValue(password)) {
+            credentials.edit().putString("id", id).putString("password", password).apply();
+            return true;
+        } else
+            return false;
     }
 }
