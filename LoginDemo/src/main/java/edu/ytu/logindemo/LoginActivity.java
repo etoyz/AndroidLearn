36043361b2a2371880b697f89025e3965c685374
etoyz <2905226519@qq.com>
2022-03-11 10:39:25 +0800

    make main activity accessible

--- LoginDemo/src/main/java/edu/ytu/logindemo/LoginActivity.java
+++ LoginDemo/src/main/java/edu/ytu/logindemo/LoginActivity.java
@@ -2,6 +2,7 @@ package edu.ytu.logindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.agree_and_continue).setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
