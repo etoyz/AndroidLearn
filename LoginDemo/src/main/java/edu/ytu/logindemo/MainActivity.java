cab16669658ee6b036b0e2841897324ae205a7b7
etoyz <2905226519@qq.com>
2022-03-12 16:11:18 +0800

    优化样式

--- LoginDemo/src/main/java/edu/ytu/logindemo/MainActivity.java
+++ LoginDemo/src/main/java/edu/ytu/logindemo/MainActivity.java
@@ -2,7 +2,6 @@ package edu.ytu.logindemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -31,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.wechat_home, R.id.address_book, R.id.find, R.id.me)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
