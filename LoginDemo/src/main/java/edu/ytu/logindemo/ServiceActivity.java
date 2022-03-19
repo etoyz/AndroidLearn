be93ff22768bbc5656f5b54058c56c477cc6ef8e
etoyz <2905226519@qq.com>
2022-03-19 11:34:52 +0800

    set activity for service page's layout file

---
+++ LoginDemo/src/main/java/edu/ytu/logindemo/ServiceActivity.java
@@ -0,0 +1,16 @@
+package edu.ytu.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceActivity extends AppCompatActivity {
+    private static MyApplication application;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        application = (MyApplication) getApplication();
+        setContentView(R.layout.activity_services);
+    }
+}
