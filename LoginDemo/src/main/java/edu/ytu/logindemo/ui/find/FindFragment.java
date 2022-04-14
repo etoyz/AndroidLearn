33c624c8baca96d01aa85ad5423ec05d76c90705
etoyz <2905226519@qq.com>
2022-04-14 12:02:28 +0800

    ::spark chronometer组件

--- LoginDemo/src/main/java/edu/ytu/logindemo/ui/find/FindFragment.java
+++ LoginDemo/src/main/java/edu/ytu/logindemo/ui/find/FindFragment.java
@@ -4,10 +4,11 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Chronometer;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,6 +37,7 @@ public class FindFragment extends Fragment {
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // 日期选择按钮
         view.findViewById(R.id.dp_btn).setOnClickListener(v -> {
             DatePickerDialog dialog = new DatePickerDialog(getContext());
             dialog.setTitle("请选择日期");
@@ -43,22 +45,60 @@ public class FindFragment extends Fragment {
             dialog.setOnDateSetListener((view1, year, month, dayOfMonth) -> application.showToast(v.getContext(), year + "年" + month + "月" + dayOfMonth + "日！", 1000));
         });
 
+        // 时间选择按钮
         view.findViewById(R.id.tp_btn).setOnClickListener(v -> {
             TimePickerDialog dialog = new TimePickerDialog(getContext(), (view12, hourOfDay, minute) -> {
                 application.showToast(v.getContext(), hourOfDay + "时" + minute + "分！", 1000);
-//                Toast toast = new Toast(getContext());
-//                toast.setView(LayoutInflater.from(v.getContext()).inflate(R.layout.toast_wechat, view.findViewById(android.R.id.content)));
-//                toast.setText("123");
-//                toast.show();
             }, 0, 0, true);
             dialog.setTitle("请选择时间");
             dialog.show();
         });
 
+        // 秒表按钮
         view.findViewById(R.id.cm_btn).setOnClickListener(v -> {
+            // 弹窗
             View cmDialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.layout_chronometer, view.findViewById(android.R.id.content), false);
             AlertDialog dialog = new AlertDialog.Builder(view.getContext()).setView(cmDialogView).create();
             dialog.show();
+            // 秒表组件
+            Chronometer chronometer = cmDialogView.findViewById(R.id.cm);
+            chronometer.setFormat("%s");
+            // 秒表按钮事件
+            class ChronometerEventsListener implements View.OnClickListener {
+                private long tmp = 0;
+
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.start:
+                            if (tmp > 0) {
+                                chronometer.setBase(chronometer.getBase() + (SystemClock.elapsedRealtime() - tmp));
+                                chronometer.start();
+                            } else {
+                                chronometer.setBase(SystemClock.elapsedRealtime());
+                                chronometer.start();
+                            }
+                            break;
+                        case R.id.pause:
+                            chronometer.stop();
+                            tmp = SystemClock.elapsedRealtime();
+                            break;
+                        case R.id.restart:
+                            chronometer.setBase(SystemClock.elapsedRealtime());
+                            chronometer.start();
+                            break;
+                        case R.id.stop:
+                            chronometer.stop();
+                            tmp = 0;
+                            break;
+                    }
+                }
+            }
+            View.OnClickListener listener = new ChronometerEventsListener();
+            cmDialogView.findViewById(R.id.start).setOnClickListener(listener);
+            cmDialogView.findViewById(R.id.pause).setOnClickListener(listener);
+            cmDialogView.findViewById(R.id.stop).setOnClickListener(listener);
+            cmDialogView.findViewById(R.id.restart).setOnClickListener(listener);
         });
     }
 }
