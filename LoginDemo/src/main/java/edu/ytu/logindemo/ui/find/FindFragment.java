47950fb26884968cbb1f48cfb732c0db8ec4ae17
etoyz <2905226519@qq.com>
2022-04-19 21:08:26 +0800

    [FEAT] music player

--- LoginDemo/src/main/java/edu/ytu/logindemo/ui/find/FindFragment.java
+++ LoginDemo/src/main/java/edu/ytu/logindemo/ui/find/FindFragment.java
@@ -2,6 +2,8 @@ package edu.ytu.logindemo.ui.find;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -9,14 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Chronometer;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import edu.ytu.logindemo.MyApplication;
 import edu.ytu.logindemo.R;
@@ -105,13 +110,43 @@ public class FindFragment extends Fragment {
             cmDialogView.findViewById(R.id.restart).setOnClickListener(listener);
         });
 
-        // 播放器
+        // 初始化播放器
+        AssetFileDescriptor assetFileDescriptor;
+        MediaPlayer player = new MediaPlayer();
+        try {
+            assetFileDescriptor = getResources().getAssets().openFd("light.mp3");
+            player.setDataSource(assetFileDescriptor.getFileDescriptor(),
+                    assetFileDescriptor.getStartOffset(),
+                    assetFileDescriptor.getLength());
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AtomicBoolean isPlaying = new AtomicBoolean(false); // 是否正在播放
+        // 打开播放器
         view.findViewById(R.id.player_btn).setOnClickListener(v -> {
             // 弹窗
-            View playerDialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.layout_player, new RelativeLayout(getContext()), false);
-            AlertDialog dialog = new AlertDialog.Builder(view.getContext()).setView(playerDialogView).create();
-            dialog.show();
+            View playerDialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.layout_player, new RelativeLayout(getContext()));
+            AlertDialog playerDialog = new AlertDialog.Builder(view.getContext()).setView(playerDialogView).create();
+            playerDialog.show();
+            playerDialog.getWindow().setLayout(400, 400);
 
+            ImageButton playBtn = playerDialog.findViewById(R.id.play);
+            ImageButton stopBtn = playerDialog.findViewById(R.id.stop);
+            playBtn.setOnClickListener((view1) -> {
+                if (!isPlaying.get()) { // 未播放
+                    isPlaying.set(true);
+                    playBtn.setImageDrawable(getContext().getDrawable(R.drawable.btn_pause));
+                    player.start();
+                } else { // 正在播放
+                    isPlaying.set(false);
+                    playBtn.setImageDrawable(getContext().getDrawable(R.drawable.btn_play));
+                    player.pause();
+                }
+            });
+//            stopBtn.setOnClickListener(v1 -> {
+//                player.stop();
+//            });
         });
     }
 }
