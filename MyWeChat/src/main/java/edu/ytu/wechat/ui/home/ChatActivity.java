816fff8962422ee869d5a512220c7e64b0402ad5
etoyz <2905226519@qq.com>
2022-05-08 07:55:18 +0800

    [OPT] 优化聊天页面操作逻辑，以及代码结构

--- MyWeChat/src/main/java/edu/ytu/wechat/ui/home/ChatActivity.java
+++ MyWeChat/src/main/java/edu/ytu/wechat/ui/home/ChatActivity.java
@@ -17,70 +17,87 @@ import edu.ytu.wechat.databinding.ActivityChatBinding;
 public class ChatActivity extends AppCompatActivity {
     private static MyApplication application;
     private static boolean isDrawerOpen = false;
+    private static int position;
+    ActivityChatBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         application = (MyApplication) getApplication();
-        ActivityChatBinding binding = ActivityChatBinding.inflate(getLayoutInflater());
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        position = getIntent().getIntExtra("position", -1);
         setContentView(binding.getRoot());
 
-        int position = getIntent().getIntExtra("position", -1);
         binding.title.setText(position + "");
-        binding.iconBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        binding.iconBtn.setOnClickListener(v -> {
 //                binding.editArea.scrollBy(0, 30); // 滑动效果
 
-                // 初始化输入区和表情抽屉的布局参数
-                RelativeLayout.LayoutParams drawerParams = new RelativeLayout.LayoutParams(binding.bottomDrawer.getLayoutParams());
-                RelativeLayout.LayoutParams editAreaParams = new RelativeLayout.LayoutParams(binding.editArea.getLayoutParams());
-                // 调整输入区和表情抽屉的布局参数
-                if (!isDrawerOpen) { // 抽屉未打开
-                    editAreaParams.addRule(RelativeLayout.ABOVE, binding.bottomDrawer.getId());
-                    drawerParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-                    binding.bottomDrawer.setVisibility(View.VISIBLE);
-                    isDrawerOpen = true;
-                } else {
-                    editAreaParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-                    binding.bottomDrawer.setVisibility(View.GONE);
-                    isDrawerOpen = false;
+            alterBottomDrawer(0);
+            binding.iconViewpager.setAdapter(new FragmentStateAdapter(getSupportFragmentManager(), getLifecycle()) {
+                @NonNull
+                @Override
+                public Fragment createFragment(int position) {
+                    Fragment fragment = new IconGridFragment();
+                    Bundle args = new Bundle();
+                    args.putInt("position", position);
+                    fragment.setArguments(args);
+                    return fragment;
                 }
-                // 应用输入区和表情抽屉的布局参数
-                binding.bottomDrawer.setLayoutParams(drawerParams);
-                binding.editArea.setLayoutParams(editAreaParams);
 
-
-                //
-                binding.iconViewpager.setAdapter(new FragmentStateAdapter(getSupportFragmentManager(), getLifecycle()) {
-                    @NonNull
-                    @Override
-                    public Fragment createFragment(int position) {
-                        Fragment fragment = new IconGridFragment();
-                        Bundle args = new Bundle();
-                        args.putInt("position", position);
-                        fragment.setArguments(args);
-                        return fragment;
-                    }
-
-                    @Override
-                    public int getItemCount() {
-                        return 2;
-                    }
-                });
-                new TabLayoutMediator(binding.iconTabs, binding.iconViewpager,
-                        (tab, position) -> {
-                            if (position == 0) {
-                                tab.setText("我的收藏");
-                            } else if (position == 1) {
-                                tab.setText("表情商店");
-                            }
+                @Override
+                public int getItemCount() {
+                    return 2;
+                }
+            });
+            new TabLayoutMediator(binding.iconTabs, binding.iconViewpager,
+                    (tab, position) -> {
+                        if (position == 0) {
+                            tab.setText("我的收藏");
+                        } else if (position == 1) {
+                            tab.setText("表情商店");
                         }
-                ).attach();
+                    }
+            ).attach();
 //                binding.iconTabs.setupWithViewPager(binding.iconViewpager);
-            }
         });
+        binding.back.setOnClickListener(v -> onBackPressed());
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (isDrawerOpen) {
+            alterBottomDrawer(2);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * open or close the bottom drawer
+     *
+     * @param mode 0:auto 1:open 2:close
+     */
+    public void alterBottomDrawer(int mode) {
+        if (mode == 0) // auto
+            mode = isDrawerOpen ? 2 : 1;
+
+        // 初始化输入区和表情抽屉的布局参数
+        RelativeLayout.LayoutParams drawerParams = new RelativeLayout.LayoutParams(binding.bottomDrawer.getLayoutParams());
+        RelativeLayout.LayoutParams editAreaParams = new RelativeLayout.LayoutParams(binding.editArea.getLayoutParams());
+        // 调整输入区和表情抽屉的布局参数
+        if (mode == 1) {
+            editAreaParams.addRule(RelativeLayout.ABOVE, binding.bottomDrawer.getId());
+            drawerParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            binding.bottomDrawer.setVisibility(View.VISIBLE);
+            isDrawerOpen = true;
+        } else {
+            editAreaParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            binding.bottomDrawer.setVisibility(View.GONE);
+            isDrawerOpen = false;
+        }
+        // 应用输入区和表情抽屉的布局参数
+        binding.bottomDrawer.setLayoutParams(drawerParams);
+        binding.editArea.setLayoutParams(editAreaParams);
     }
 }
