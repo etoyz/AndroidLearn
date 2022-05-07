b6b660f5d1865c7ca5d050557a7c7e16017c507c
etoyz <2905226519@qq.com>
2022-05-07 17:19:42 +0800

    [FEAT] 表情包抽屉

--- MyWeChat/src/main/java/edu/ytu/wechat/ui/home/ChatActivity.java
+++ MyWeChat/src/main/java/edu/ytu/wechat/ui/home/ChatActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import edu.ytu.wechat.MyApplication;
 import edu.ytu.wechat.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity {
     private static MyApplication application;
+    private static boolean isDrawerOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,23 @@ public class ChatActivity extends AppCompatActivity {
         binding.iconBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                binding.editArea.scrollBy(0, 30);
+//                binding.editArea.scrollBy(0, 30); // 滑动效果
+
+                // 初始化输入区和表情抽屉的布局参数
+                RelativeLayout.LayoutParams drawerParams = new RelativeLayout.LayoutParams(binding.bottomDrawer.getLayoutParams());
+                RelativeLayout.LayoutParams editAreaParams = new RelativeLayout.LayoutParams(binding.editArea.getLayoutParams());
+                if (!isDrawerOpen) { // 抽屉未打开
+                    editAreaParams.addRule(RelativeLayout.ABOVE, binding.bottomDrawer.getId());
+                    drawerParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+                    binding.bottomDrawer.setVisibility(View.VISIBLE);
+                    isDrawerOpen = true;
+                } else {
+                    editAreaParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+                    binding.bottomDrawer.setVisibility(View.GONE);
+                    isDrawerOpen = false;
+                }
+                binding.bottomDrawer.setLayoutParams(drawerParams);
+                binding.editArea.setLayoutParams(editAreaParams);
             }
         });
 
