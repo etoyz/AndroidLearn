330dfeb7015f43c0adc32e7708a39ba090bf5012
etoyz <2905226519@qq.com>
2022-05-07 21:25:17 +0800

    [FEAT] 表情包gridview

--- MyWeChat/src/main/java/edu/ytu/wechat/ui/home/IconGridFragment.java
+++ MyWeChat/src/main/java/edu/ytu/wechat/ui/home/IconGridFragment.java
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
+import edu.ytu.wechat.R;
 import edu.ytu.wechat.databinding.FragmentIconGridBinding;
 
 public class IconGridFragment extends Fragment {
@@ -27,6 +30,44 @@ public class IconGridFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentIconGridBinding.inflate(inflater, container, false);
         binding.title.setText("Tab" + position);
+
+        binding.iconGrid.setNumColumns(4);
+        binding.iconGrid.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return (position + 1) * 3; // TODO
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ViewHolder holder;
+                if (convertView == null) {
+                    convertView = inflater.inflate(R.layout.fragment_icon_grid_item, parent, false);
+                    holder = new ViewHolder();
+                    holder.img = convertView.findViewById(R.id.grid_iv);
+                    convertView.setTag(holder);
+                } else {
+                    holder = (ViewHolder) convertView.getTag();
+                }
+                // 通过 holder 修改 View
+                holder.img.setImageResource(R.drawable.avatar);
+                return convertView;
+            }
+
+            class ViewHolder {
+                ImageView img;
+            }
+        });
         return binding.getRoot();
     }
 }
