fcc02f423f8a1141cb9ccab97e3213880f505fb8
etoyz <2905226519@qq.com>
2022-06-01 15:01:47 +0800

    fix bug

--- MyWeChat/src/main/java/edu/ytu/wechat/ui/home/ChatListAdapter.java
+++ MyWeChat/src/main/java/edu/ytu/wechat/ui/home/ChatListAdapter.java
@@ -3,6 +3,7 @@ package edu.ytu.wechat.ui.home;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -49,29 +50,37 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ViewHo
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public void setChatMessage(ChatMessage chatMessage) {
             this.chatMessage = chatMessage;
-            if(chatMessage.getContent().equals(""))
+            if (chatMessage.getContent().equals(""))
                 chatMessage.setContent("      ");
             if (chatMessage.isYours()) {
                 msgYours.setText(chatMessage.getContent());
                 // 此ViewHolder如果是回收来的，”msgYours“可能之前被设置为不可见
                 msgYours.setVisibility(View.VISIBLE);
+                avatarYours.setVisibility(View.VISIBLE);
                 msgOther.setVisibility(View.GONE);
+                avatarOther.setVisibility(View.GONE);
             } else {
                 msgOther.setText(chatMessage.getContent());
                 // 此ViewHolder如果是回收来的，“msgOther”可能之前被设置为不可见
                 msgOther.setVisibility(View.VISIBLE);
+                avatarOther.setVisibility(View.VISIBLE);
                 msgYours.setVisibility(View.GONE);
+                avatarYours.setVisibility(View.GONE);
             }
         }
 
         public ChatMessage chatMessage;
         private final TextView msgOther;
         private final TextView msgYours;
+        private final ImageView avatarYours;
+        private final ImageView avatarOther;
 
         public ViewHolder(ActivityChatItemBinding binding) {
             super(binding.getRoot());
             msgOther = binding.msgOther;
             msgYours = binding.msgYours;
+            avatarYours = binding.avatarYours;
+            avatarOther = binding.avatarOther;
         }
     }
 }
