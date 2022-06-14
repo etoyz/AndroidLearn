5bd62b7de4064fe51c142e0ba6ba31efbccb3e43
etoyz <2905226519@qq.com>
2022-06-14 16:48:30 +0800

    [OPT] 表情

--- MyWeChat/src/main/java/edu/ytu/wechat/ui/home/IconGridFragment.java
+++ MyWeChat/src/main/java/edu/ytu/wechat/ui/home/IconGridFragment.java
@@ -29,13 +29,15 @@ public class IconGridFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentIconGridBinding.inflate(inflater, container, false);
-        binding.title.setText("Tab" + position);
 
         binding.iconGrid.setNumColumns(4);
         binding.iconGrid.setAdapter(new BaseAdapter() {
             @Override
             public int getCount() {
-                return (position + 1) * 3; // TODO
+                if (position == 0)
+                    return 3;
+                else
+                    return 5;
             }
 
             @Override
@@ -49,7 +51,7 @@ public class IconGridFragment extends Fragment {
             }
 
             @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
+            public View getView(int p, View convertView, ViewGroup parent) {
                 ViewHolder holder;
                 if (convertView == null) {
                     convertView = inflater.inflate(R.layout.fragment_icon_grid_item, parent, false);
@@ -60,7 +62,24 @@ public class IconGridFragment extends Fragment {
                     holder = (ViewHolder) convertView.getTag();
                 }
                 // 通过 holder 修改 View
-                holder.img.setImageResource(R.drawable.avatar);
+                switch (p) {
+                    case 0:
+                        holder.img.setImageResource(R.drawable.a01);
+                        break;
+                    case 1:
+                        holder.img.setImageResource(R.drawable.a02);
+                        break;
+                    case 2:
+                        holder.img.setImageResource(R.drawable.a03);
+                        break;
+                    case 3:
+                        holder.img.setImageResource(R.drawable.a04);
+                        break;
+                    case 4:
+                        holder.img.setImageResource(R.drawable.a05);
+                        break;
+                }
+
                 return convertView;
             }
 
